2e61937d2f36b6002eb55d618b2a31f42a9fef0e
cindyli-code <70749579+cindyli-code@users.noreply.github.com>
2020-11-26 17:38:04 +0800

    Update jk.py

--- jk.py
+++ jk.py
@@ -1,6 +1,6 @@
 import pymongo
 
-uri="mongodb://automationuser:everbridge@127.0.0.1:27017/ebsautomation"
+uri="mongodb://automationuser:everbridge@0.0.0.0:27017/ebsautomation"
 print("test connection")
 a=pymongo.MongoClient(uri, connect=False)
 print(a.server_info())
