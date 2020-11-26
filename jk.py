83fa092cb6041dd7b9419fbda398a5407c2bcd64
cindyli-code <70749579+cindyli-code@users.noreply.github.com>
2020-11-26 17:06:52 +0800

    Update jk.py

--- jk.py
+++ jk.py
@@ -2,4 +2,5 @@ import pymongo
 
 uri="mongodb://root:Not1f1cat1on@127.0.0.1:27001/admin"
 print("test connection")
-pymongo.MongoClient(uri, connect=False)
+a=pymongo.MongoClient(uri, connect=False)
+print(a.server_info())
